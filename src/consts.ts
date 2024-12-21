9c9ca5055fb059aa2c9c3d8e3a95a8df3c6d2f26
ChinmayNoob <chinmaysk0408@gmail.com>
2024-12-21 22:14:06 +0530

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_URL = "https://localhost:4321";
+export const SITE_TITLE = "Chinmay's Blog";
+export const SITE_DESCRIPTION =
+    "A personal blogging site to share my thoughts, built with Astro and Tailwind CSS for a clean, seamless experience.";
+
+export const EMAIL = "chinmaypvt04@gmail.com";
