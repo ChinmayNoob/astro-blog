f8d7699ef7549ddc3ba0502e59d5b7473587ca20
ChinmayNoob <chinmaysk0408@gmail.com>
2025-07-06 13:12:05 +0530

    fixing some changes

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,4 @@ export const SITE_TITLE = "Chinmay's Blog";
 export const SITE_DESCRIPTION =
     "A personal blogging site to share my thoughts, built with Astro and Tailwind CSS for a clean, seamless experience.";
 
-export const EMAIL = "chinmaypvt04@gmail.com";
+export const EMAIL = "chinmay.sawant4804@gmail.com";
